e3e913810aa6a4036405d4e6d896692cded6a80c
defuera <justdevelopers@gmail.com>
2020-10-07 12:25:47 +0200

    Initial commit

---
+++ lib/dart_obfuscator.dart
@@ -0,0 +1,9 @@
+library dart_obfuscator;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
+
+
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:path/path.dart' as p;
+
+void main () {
+  final path = p.join('directory', 'file.txt');
+
+  print(6 * 7 ~/ 2);
+  print("path $path");
+}
