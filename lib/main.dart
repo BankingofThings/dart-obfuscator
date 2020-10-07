d570559dc0c24ae541d2bcbb0f3589ca2c271dc7
defuera <justdevelopers@gmail.com>
2020-10-07 16:36:45 +0200

    Implement update imports to absolute path

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'dart:io';
 
 import 'package:dart_obfuscator/log_level.dart';
+import 'package:path/path.dart';
 
-// 1. Read file structure
-// 2. Define exported files that should not be modified
-// 3. Define files that should be modified
+// 1. Move file to output
+// 2. keep all the package imports in the file
+// 3. update all relative imports in the file
+// 4. In other files update all references to moved file
 
-// final logLevel = LogLevel.DEBUG;
-final logLevel = LogLevel.VERBOSE;
+final logLevel = LogLevel.DEBUG;
+// final logLevel = LogLevel.VERBOSE;
+final obfuscationMap = Map<String, String>();
+final packageName = 'finn_dart_sdk';
 
 void main(List<String> args) async {
   final sourceDirPath = "/Users/denisvolyntsev/dev/Finn/Dart-Mobile-SDK";
+  final outputFileName = "finn_obfuscated.dart";
   final libDir = Directory("$sourceDirPath/lib");
 
   if (!libDir.existsSync()) {
@@ -22,19 +27,51 @@ void main(List<String> args) async {
   final rawFiles = rootFiles + exportedFiles;
 
   if (logLevel == LogLevel.VERBOSE) print("Files to obfuscate");
-  List filesToObfuscate = findFilesToObfuscate(libDir, rawFiles);
+  List<File> filesToObfuscate = findFilesToObfuscate(libDir, rawFiles);
   filesToObfuscate.forEach((element) {
     if (logLevel == LogLevel.VERBOSE) print("${element.path}");
   });
 
+  final outputFile = File("${libDir.path}/$outputFileName");
+  outputFile.createSync();
+
+  var theFile = filesToObfuscate[0];
+  final updatedFileContent = updateImportsToAbsolute(libDir, theFile);
+  // 2. keep all the package imports in the file
+// 3. update all relative imports in the file
+  // 1. Move file to output and delete original file
+  outputFile.writeAsStringSync(updatedFileContent);
+  // theFile.delete(recursive: false);
+
+// 4. In other files update all references to moved file
+}
+
+String updateImportsToAbsolute(Directory libDir, File theFile) {
+  var thePath = theFile.path;
+  thePath = thePath.replaceAll(basename(thePath), "");
+  // print(theFile.readAsLinesSync());
+  final updatedLines = theFile.readAsLinesSync().map((line) {
+    if (isLineRelativeImport(line)) {
+      final relativePath = thePath.split("/lib/").last;
+      final newLine = line.replaceAll("import '", "import 'package:$packageName/$relativePath");
+
+      print("Replace relative import:\n$line\nto:\n$newLine");
+      return newLine;
+    } else {
+      return line;
+    }
+  }).toList();
+  // print("updatedLines:\n$updatedLines");
+  return updatedLines.reduce((value, element) => value + "$element\n");
 }
 
 List<File> findFilesToObfuscate(Directory libDir, List<FileSystemEntity> rawFiles) {
-  return libDir.listSync(recursive: true)
-    .whereType<File>()
-    .where((element) => element.path.split(".").last == "dart")
-    .where((element) => !rawFiles.map((e) => e.path).contains(element.path))
-    .toList();
+  return libDir
+      .listSync(recursive: true)
+      .whereType<File>()
+      .where((element) => element.path.split(".").last == "dart")
+      .where((element) => !rawFiles.map((e) => e.path).contains(element.path))
+      .toList();
 }
 
 List<File> findExportedFiles(List<FileSystemEntity> rootFiles) {
@@ -78,12 +115,18 @@ List<File> checkExports(File file) {
 
 /// Receives import or export line and returns cleared path.
 String clearImportSymbols(String line) {
-  if (line.startsWith('import ')) {
+  if (isLineImport(line)) {
     return line.replaceAll('import \'', '').replaceAll("\'", "").replaceAll('\;', "");
-  } else if (line.startsWith('export ')) {
+  } else if (isLineExport(line)) {
     var clear = line.replaceAll('export \'', '').replaceAll("\'", "").replaceAll('\;', "");
     return clear;
   } else {
     throw "This is neither import nor export line";
   }
 }
+
+bool isLineExport(String line) => line.startsWith('export ');
+
+bool isLineImport(String line) => line.startsWith('import ');
+
+bool isLineRelativeImport(String line) => isLineImport(line) && !line.contains("'package:") && !line.startsWith("import 'dart:");
