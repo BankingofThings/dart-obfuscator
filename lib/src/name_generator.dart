1eb181f88d5934f1c8a16f5a610d8756030b5461
Brett Sutton <bsutton@noojee.com.au>
2023-11-17 19:08:40 +1100

    finally fixed the stackover flow in the name generator

--- lib/src/name_generator.dart
+++ lib/src/name_generator.dart
@@ -19,8 +19,8 @@ class IdentifierNameGenerator {
   int _counter = 0;
 
   String next() {
+    ++_counter;
     final identifierName = _generateIdentifier(_counter);
-    _counter++;
     return identifierName;
   }
 
