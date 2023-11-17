00d789e15b3491e266b295589751bff70f864284
Brett Sutton <bsutton@noojee.com.au>
2023-11-17 18:36:31 +1100

    added a check that we don't generate a reservered word as an identifier name.

--- lib/src/name_generator.dart
+++ lib/src/name_generator.dart
@@ -15,26 +15,99 @@ class NameGenerator {
   }
 }
 
-class VariableNameGenerator {
+class IdentifierNameGenerator {
   int _counter = 0;
 
   String next() {
-    final variableName = _generateVariableName(_counter);
+    final identifierName = _generateIdentifier(_counter);
     _counter++;
-    return variableName;
+    return identifierName;
   }
 
-  String _generateVariableName(int index) {
+  String _generateIdentifier(int index) {
     const base = 26; // Number of letters in the alphabet
-    var variableName = '';
+    var identifierName = '';
 
     do {
       final remainder = index % base;
-      variableName =
-          String.fromCharCode('a'.codeUnitAt(0) + remainder) + variableName;
+      identifierName =
+          String.fromCharCode('a'.codeUnitAt(0) + remainder) + identifierName;
       index = (index ~/ base) - 1;
     } while (index >= 0);
 
-    return variableName;
+    if (reservedWords.contains(identifierName)) {
+      return next();
+    }
+    return identifierName;
   }
+
+  static const reservedWords = [
+    'abstract',
+    'as',
+    'assert',
+    'async',
+    'await',
+    'base',
+    'break',
+    'case',
+    'catch',
+    'class',
+    'const',
+    'continue',
+    'covariant',
+    'default',
+    'deferred',
+    'do',
+    'dynamic',
+    'else',
+    'enum',
+    'export',
+    'extends',
+    'extension',
+    'external',
+    'factory',
+    'false',
+    'final',
+    'final',
+    'finally',
+    'for',
+    'function',
+    'get',
+    'hide',
+    'if',
+    'implements',
+    'import',
+    'in',
+    'interface',
+    'is',
+    'late',
+    'library',
+    'mixin',
+    'new',
+    'null',
+    'on',
+    'operator',
+    'part',
+    'required',
+    'rethrow',
+    'return',
+    'sealed',
+    'set',
+    'show',
+    'static',
+    'super',
+    'switch',
+    'sync',
+    'this',
+    'throw',
+    'true',
+    'try',
+    'typedef',
+    'var',
+    'void',
+    'when',
+    'while',
+    'with',
+    'yield',
+  ];
 }
