b7a87b33e33e5de0251bab68c97051fd295eeb55
Brett Sutton <bsutton@noojee.com.au>
2023-11-23 14:24:45 +1100

    wip on adding the abilty to include/exclude libraries.

--- lib/src/obfuscated_project.dart
+++ lib/src/obfuscated_project.dart
@@ -1,27 +1,39 @@
+import 'dart:io';
+
 import 'package:codemod_core/codemod_core.dart';
 import 'package:dcli/dcli.dart';
+import 'package:glob/glob.dart';
 import 'package:meta/meta.dart';
 import 'package:path/path.dart';
 
 import 'exceptions.dart';
+import 'obfuscating_visitor.dart';
+import 'project_context.dart';
 import 'replacer.dart';
-import 'visitor.dart';
 
 /// Used to create the obfuscated version of the project.
 
 class ObfuscatedProject implements ProjectContext {
   /// The obfuscated version of the project is saved to
   /// [pathToTargetProject]
+  /// You can limit the set of dart files by passing in a set of filters
+  /// in the forms of globs that files must match at least one of to be
+  /// included.  If [includes] is empty then all files in match.
   ObfuscatedProject(
-      {required this.pathToSourceProject, required this.pathToTargetProject});
+      {required this.pathToSourceProject,
+      required this.pathToTargetProject,
+      required this.includes,
+      required this.excludes});
 
   final String pathToSourceProject;
+  final List<String> includes;
+  final List<String> excludes;
   final String pathToTargetProject;
 
   /// List of dart libraries that are part of the project.
   /// We use this to identify internal and external calls
   /// as we can't obfuscate calls to external libraries.
-  Set<Path> _libraries = <Path>{};
+  final Set<Path> _libraries = <Path>{};
 
   ///
   /// Prepare the code for obfuscation by copying it
@@ -30,29 +42,74 @@ class ObfuscatedProject implements ProjectContext {
   ///
   void prepare({required bool overwrite}) {
     // Preparation
+    _prepareDirectory(overwrite);
+
+    /// Run pub get to ensure the AST is fully resolved.
+    print('running dart pub get');
+    DartSdk().runPubGet(pathToTargetProject);
+
+    final paths =
+        find('*.dart', workingDirectory: pathToTargetProject).toList().toSet();
+
+    if (includes.isEmpty && excludes.isEmpty) {
+      _libraries.addAll(paths);
+    } else {
+      _libraries.addAll(paths.where(_allowed));
+    }
+  }
+
+  /// Create the output directory and copy the core project
+  /// files into it.
+  void _prepareDirectory(bool overwrite) {
+    // validate that the output directory is sensible.
+    if (equals(pathToTargetProject, rootPath)) {
+      printerr(red('The output directory may not be $rootPath'));
+      exit(1);
+    }
+    if (split(pathToTargetProject).length < 2) {
+      printerr(red('The output directory may not be in $pathToTargetProject'));
+      exit(1);
+    }
+
     if (exists(pathToTargetProject)) {
       if (!overwrite) {
         throw StructureException(
             '''The target directory $pathToTargetProject exists. Delete it or use the --overwrite option.''');
       }
 
-      deleteDir(pathToTargetProject);
+      _deleteOutputDir(pathToTargetProject);
     }
     createDir(pathToTargetProject, recursive: true);
     _copyCoreStructure();
+  }
 
-    /// Run pub get to ensure the AST is fully resolved.
-    print('running dart pub get');
-    DartSdk().runPubGet(pathToTargetProject);
+  /// [path] will be obfuscated providing it matches at least
+  /// one of the [includes]s.
+  bool _allowed(String path) {
+    /// Globs are relative to the target project root
+    final relativePath = relative(path, from: pathToTargetProject);
+    if (relativePath.contains('bin')) {
+      print('hi');
+    }
 
-    _libraries =
-        find('*.dart', workingDirectory: join(pathToTargetProject, 'lib'))
-            .toList()
-            .toSet();
+    for (final exclude in excludes) {
+      if (Glob(exclude).matches(relativePath)) {
+        return false;
+      }
+    }
 
-    _libraries.addAll(
-        find('*.dart', workingDirectory: join(pathToTargetProject, 'test'))
-            .toList());
+    /// If there are no includes then everything not excluded
+    /// is included.
+    if (includes.isEmpty) {
+      return true;
+    }
+
+    for (final include in includes) {
+      if (Glob(include).matches(relativePath)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   ///
@@ -72,7 +129,7 @@ class ObfuscatedProject implements ProjectContext {
       required String pathToSourceParent,
       required String pathToTargetParent}) async {
     final pg = PatchGenerator([
-      Visitor(context).call,
+      ObfuscatingVisitor(context).call,
     ]);
 
     /// do we need to pass a stream to the generator given how big our list
@@ -164,11 +221,27 @@ class ObfuscatedProject implements ProjectContext {
       _libraries.contains(pathToLibrary);
 
   @override
-  String replace(String name) => Replacer().replace(name);
-}
+  Replacement replace(String name) => Replacer().replace(name);
 
-abstract interface class ProjectContext {
-  String replace(String name);
+  /// Returns the list of libraries that are being obfuscated.
+  @override
+  Set<String> get libraries => _libraries;
 
-  bool isLocalLibrary(String pathToLibrary);
+  /// Delete the contents of the output director.
+  /// We retain the root directory as it make it easier to
+  /// have an editor open in the root of the output directory
+  /// to monitor results.
+  void _deleteOutputDir(String pathToTargetProject) {
+    final children = find('*', includeHidden: true).toList();
+
+    for (final child in children) {
+      if (isDirectory(child)) {
+        deleteDir(child);
+      } else if (isLink(child)) {
+        deleteSymlink(child);
+      } else {
+        delete(child);
+      }
+    }
+  }
 }
