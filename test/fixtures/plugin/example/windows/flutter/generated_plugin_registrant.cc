9db94e82458f44f8ddbc945e9a71cebe8d5490a5
Brett Sutton <bsutton@noojee.com.au>
2023-11-23 14:38:54 +1100

    added platform specific generated files for the example.

---
+++ test/fixtures/plugin/example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <plugin/plugin_c_api.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  PluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("PluginCApi"));
+}
